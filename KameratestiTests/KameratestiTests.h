1a9e386826f89298dc037ffe5fab308c609aba0b
Tuomo Lassila <Topis@Tuomos-MacBook-Pro.local>
2012-05-08 18:31:25 +0300

    Initial Commit

---
+++ KameratestiTests/KameratestiTests.h
@@ -0,0 +1,13 @@
+//
+//  KameratestiTests.h
+//  KameratestiTests
+//
+//  Created by Tuomo Lassila on 5/8/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface KameratestiTests : SenTestCase
+
+@end
---
+++ KameratestiTests/KameratestiTests.m
@@ -0,0 +1,32 @@
+//
+//  KameratestiTests.m
+//  KameratestiTests
+//
+//  Created by Tuomo Lassila on 5/8/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "KameratestiTests.h"
+
+@implementation KameratestiTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in KameratestiTests");
+}
+
+@end
